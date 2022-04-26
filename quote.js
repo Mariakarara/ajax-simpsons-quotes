82cb93a536d0029c90fd36fd80b4261fadead2a5
Mariakarara <claind15@yahoo.fr>
2022-04-26 19:21:45 +0200

    ajout bouton quote

--- quote.js
+++ quote.js
@@ -1,40 +1,46 @@
-// This function loads quotes data from the Simpson API
-function fetchSimpsonJSON() {
+//  This function loads quotes data from the Simpson API
+let currQuote = 1;
+const num = 10;
+
+const nextQuote = () => {
+    currQuote++;
+    if (currQuote >= num) {
+        currQuote = 1;
+    }
+
+    fetchSimpsonJSON(currQuote);
+
+}
+function fetchSimpsonJSON(currQuote) {
 
     // Feel free to download this HTML and edit it, to use another Simpson ID
-    
-    const num = 10;
-    
-    const url = `https://simpsons-quotes-api.herokuapp.com/quotes?count=num`;
-    
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes?count=${num}`;
+
     fetch(url)
-    
-    .then(function (response) {
-    
-    return response.json(); // response.data instead of response.json() with fetch
-    
-    })
-    
-    .then(function (simpsons) {
-    
-    console.log('data decoded from JSON:', simpsons);
-    console.log('typeof simpsons:', typeof simpsons);
-    // Build a block of HTML
-    
-    const simpsonHtml = `
-    
-    <p><strong>${simpsons[0].quote}</strong></p>
-    
-    <p><strong>${simpsons[0].character}</strong></p>
-    
-    <img src="${simpsons[0].image}" />
-    
-    `;
-    
-    document.querySelector('#simpson').innerHTML = simpsonHtml;
-    
-    });
-    
-    }
-    fetchSimpsonJSON()
-    
+
+        .then(function (response) {
+
+            return response.json(); // response.data instead of response.json() with fetch
+
+        })
+
+        .then(function (simpsons) {
+            let html = '';
+            console.log('data decoded from JSON:', simpsons);
+            console.log('typeof simpsons:', typeof simpsons);
+            // Build a block of HTML
+            const simpsonHtml = `
+
+         <p><strong>${simpsons[currQuote].quote}</strong></p>
+
+          <p><strong>${simpsons[currQuote].character}</strong></p>
+
+          <img src="${simpsons[currQuote].image}" />
+        `;
+            document.querySelector('#simpson').innerHTML = simpsonHtml;
+
+        })
+
+
+}
+fetchSimpsonJSON(currQuote);
